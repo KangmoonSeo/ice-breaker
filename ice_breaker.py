d654db6af661af4661f88fc18a119eb7e01f7844
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-01-12 20:39:06 +0900

    feat: output_parser 출력 직렬화(serializable)

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,33 +1,43 @@
+from typing import Tuple
+
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
-
 from third_parties.linkedin import scrape_linked_profile
 from third_parties.twitter import scrape_user_tweets
+from output_parsers.output_parsers import person_intel_parser, PersonIntel
 
 
-def ice_break(name: str) -> str:
+def ice_break(name: str, is_twitter: bool = False) -> Tuple[PersonIntel, str]:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linked_profile(linkedin_profile_url=linkedin_profile_url)
 
-    twitter_username = twitter_lookup_agent(name=name)
-    twitter_data = scrape_user_tweets(username=twitter_username, num_tweets=5)
+    twitter_data = "no_data"
+    if is_twitter:
+        twitter_username = twitter_lookup_agent(name=name)
+        twitter_data = scrape_user_tweets(username=twitter_username, num_tweets=5)
 
     summary_template = """
     given the Linkedin information {linkedin_information} 
-    and twitter {twitter_information} about a person from I want you to create in Korean:
+    and Twitter {twitter_information} about a person from I want you to create in Korean:
     1. a short summary
     2. two interesting facts about them
     3. A topic that may interest them
     4. 2 creative Ice breakers to open a conversation with them 
+    \n{format_instructions}
     """
+    # 템플릿 끝애 format_instructions를 추가해서 output 형식을 알려줌
+    # output_parser의 추상화
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
@@ -35,10 +45,10 @@ def ice_break(name: str) -> str:
     result = chain.run(
         linkedin_information=linkedin_data, twitter_information=twitter_data
     )
-    return result
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
     print("Hello LangChain!")
-    information = ice_break(name="Eden Marco udemy")
+    information = ice_break(name="Eden Marco Udemy")
     print(information)
---
+++ output_parsers/output_parsers.py
@@ -0,0 +1,40 @@
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+"""
+pydantic 이란 무엇인가?
+파이썬의 내장 데이터 클래스와 매우 유사하지만, 실은 외부 라이브러리.
+데이터 유효성 검사와 설정 관리에 유용함
+"""
+
+from typing import List
+
+
+class PersonIntel(BaseModel):
+    summary: str = Field(description="Summary of the person")
+    facts: List[str] = Field(description="Interesting facts about the person")
+    topics_of_interest: List[str] = Field(
+        description="Topics that may interest the person"
+    )
+    ice_breakers: List[str] = Field(
+        description="Create ice breakers to open a conversation"
+    )
+
+    def to_dict(self):
+        return {
+            "summary": self.summary,
+            "facts": self.facts,
+            "topics_of_interest": self.topics_of_interest,
+            "ice_breakers": self.ice_breakers,
+        }
+
+
+"""
+input_parser를 추후에 프롬프트 템플릿에 연결함
+프롬프트 템플릿을 만들 때마다 아웃풋 파서에 연결하여
+LLM에게 어떻게 답변을 돌려받을 지 명령하게 됨
+json, xml, yaml? 
+"""
+person_intel_parser: PydanticOutputParser = PydanticOutputParser(
+    pydantic_object=PersonIntel
+)
